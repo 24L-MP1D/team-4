5356bd08c16cac09f4840b66b15ccf646a3c75ba
24LP4508 24LP4508 <24LP4508@Admins-iMac.local>
2024-09-13 15:38:58 +0800

    ProductDash

---
+++ e-commerce-frontend/src/app/components/productDash.tsx
@@ -0,0 +1,16 @@
+
+
+export function Product() {
+    return(
+        <div>
+            <div>Side</div>
+            <div>Hero
+                <div>
+                    <p>Бүтээгдэхүүн</p>
+                    <p>Ангилал</p>
+                </div>
+                <button>Бүтээгдэхүүн нэмэх</button>
+            </div>
+        </div>
+    );
+}
