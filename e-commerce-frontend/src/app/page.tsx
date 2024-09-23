75ec351db70a7c5d622f6b6469df3366b3825f06
anarchuluubayarsaikhan <anarchulu@gmail.com>
2024-09-23 19:22:31 +0800

    usercard design editing

--- e-commerce-frontend/src/app/page.tsx
+++ e-commerce-frontend/src/app/page.tsx
@@ -1,12 +1,13 @@
 import { ImageSlider } from "@/components/carousel";
 import { StoreProfile } from "@/components/storeprofile";
+import { UserCard } from "@/components/usercard";
 import { UserCards } from "@/components/usercards";
 
 
 export default function Home() {
   return (
     <div className="max-w-[1000px] m-auto flex flex-col gap-5">
-      <StoreProfile/>
+      <UserCard/>
     </div>
   )
 
--- e-commerce-frontend/src/components/usercard.tsx
+++ e-commerce-frontend/src/components/usercard.tsx
@@ -8,14 +8,20 @@ import {
 
 import Image from "next/image"
 import { Heart } from "lucide-react"
+import { useState } from "react";
+import { Button } from "./ui/button"
 
 export function UserCard () {
+    const [filled, setFilled] = useState(false);
     return (
         <div>
             <Card>
                 <CardContent className="relative overflow-hidden rounded-[16px]  max-w-[244px] h-[331px] " >
                     <Image alt="Image of product" width={245} height={331} src="/testimage.jpg" className="w-full  rounded-[16px] hover:transform hover:scale-125  h-full hover:duration-1000 aspect-video"/>
-                    <Heart strokeWidth={1} className="absolute top-4 right-4"/>
+                    <Button onClick={() => setFilled(!filled)}>
+                        <Heart strokeWidth={1} className="absolute top-4 right-4" fill={filled === true ? "red" : "white"}/>
+                    </Button>
+                    
                 </CardContent>
             </Card>
        <div className="flex flex-col gap-1">
