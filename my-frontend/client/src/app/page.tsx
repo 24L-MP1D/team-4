e1816f526edec1b4cac4e8b297d9d9b1285dcf9e
UG <110281844+ugankhuyag@users.noreply.github.com>
2024-09-24 18:08:36 +0800

    add folder

--- my-frontend/client/src/app/page.tsx
+++ my-frontend/client/src/app/page.tsx
@@ -1,12 +1,10 @@
 import { Header } from "@/components/header";
-import { Listitem } from "@/components/listitem";
-import { ProductDash } from "@/components/productDash";
+
 
 export default function Home() {
   return (
     <div className="max-w-[1440px] mx-auto">
       <Header />
-      <Listitem />
     </div>
   )
 
