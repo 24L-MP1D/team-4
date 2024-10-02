2a328af464bf0a69e449696b983025a48332431c
24LP4508 24LP4508 <you@example.com>
2024-10-02 18:07:53 +0800

    Productdetail

--- e-commerce-frontend/src/app/page.tsx
+++ e-commerce-frontend/src/app/page.tsx
@@ -1,14 +1,11 @@
 "use client"
 
 import { ProductDetail } from "@/components/clientProductDetail"
-import { UserInfo } from "@/components/userInfo";
-
 
 export default function Home() {
   return (
     <div className="max-w-[1440px] mx-auto">
       <ProductDetail />
-      <UserInfo />
     </div>
   );
 }
