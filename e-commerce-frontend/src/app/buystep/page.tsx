6e7b6369f8163a6a43357fac658e945fac05e5f6
Uuganaa-abo <aku.pbnn@gmail.com>
2024-10-11 12:20:10 +0800

    BuyStep

--- e-commerce-frontend/src/app/buystep/page.tsx
+++ e-commerce-frontend/src/app/buystep/page.tsx
@@ -1,30 +1,49 @@
+"use client";
 import BuyStepOne from "@/components/buyStepOne";
 import { BuyStepThree } from "@/components/buyStepThree";
 import { BuySteoTwo } from "@/components/buyStepTwo";
 import { Header } from "@/components/header";
-
+import { useState } from "react";
+import { IoMdCheckmark } from "react-icons/io";
 
 export default function BuyStep() {
+  const [visible, setVisible] = useState<number>(1);
   return (
     <section>
       <Header />
       <div className=" flex flex-col items-center mt-[92px]">
         <div className=" flex flex-col items-center">
           <div className="flex items-center pl">
-            <button className="rounded-full size-10 border-[1px] border-black">
-              1
-            </button>
+            <div className="rounded-full size-10 border-[1px] border-black bg-primaryBlue font-bold text-base text-white flex justify-center items-center">
+              {visible === 1 ? "1" : <IoMdCheckmark />}
+            </div>
             <div className="border-x-[80px] border-solid border-[1px] border-primary"></div>
-            <button  className="rounded-full size-10 border-[1px] border-black">
-              2
-            </button>
+            <div
+              className={`${
+                visible != 1
+                  ? "bg-primaryBlue text-bold  text-base text-white"
+                  : "bg-[#ffffff] rounded-full size-10 border-[1px] border-black flex justify-center items-center"
+              }`}
+            >
+              {visible != 3 ? "2" : <IoMdCheckmark />}
+            </div>
             <div className="border-x-[80px] border-solid border-[1px] border-primary"></div>
-            <button  className="rounded-full size-10 border-[1px] border-black">
+            <div
+              className={`${
+                visible === 3
+                  ? "bg-primaryBlue font-bold text-base text-white"
+                  : "bg-[#ffffff] rounded-full size-10 border-[1px] border-black flex justify-center items-center"
+              }`}
+            >
               3
-            </button>
+            </div>
           </div>
         </div>
-        <BuyStepOne />
+        <div>
+          {visible === 1 && <BuyStepOne setVisible={setVisible} />}
+          {visible === 2 && <BuySteoTwo setVisible={setVisible} />}
+          {visible === 3 && <BuyStepThree setVisible={setVisible} />}
+        </div>
       </div>
     </section>
   );
