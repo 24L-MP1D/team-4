5e5547e5b1819cf6edb71c06b10e608b7ef281e6
24LP4508 24LP4508 <you@example.com>
2024-10-14 15:41:43 +0800

    ProsuctDetail

--- e-commerce-frontend/src/components/clientProductDetail.tsx
+++ e-commerce-frontend/src/components/clientProductDetail.tsx
@@ -6,10 +6,37 @@ import { useEffect, useState } from "react";
 import Image from "next/image";
 import { Separator } from "@/components/ui/separator";
 import { Textarea } from "./ui/textarea";
+<<<<<<< HEAD
 import { useFormik, FormikErrors } from "formik";
 import { usePathname } from "next/navigation";
 interface FormValues {
   value: string;
+=======
+import { title } from "process";
+import { useFormik, FormikErrors } from "formik";
+import { error } from "console";
+
+interface FormValues {
+  title: string;
+}
+
+//  function CreatComment(){
+
+// }
+function submit() {
+  fetch(`http://backend/productDetail`, {
+    method: "POST",
+    body: JSON.stringify({
+      title: title,
+    }),
+  }).then((res) => {
+    if (res.ok) {
+      // Success
+    } else {
+      // error
+    }
+  });
+>>>>>>> 59652ed (Formik)
 }
 export type Products = {
   _id: string,
@@ -45,6 +72,7 @@ export function ProductDetail() {
     },
   });
   const [visible, SetVisible] = useState(false);
+<<<<<<< HEAD
   const [comment, SetComment] = useState("");
   const [data, setData] = useState<Products[]>([]);
   const [selected, SetSelected] = useState("");
@@ -214,6 +242,152 @@ export function ProductDetail() {
               </Button>
             </div>
 =======
+=======
+  // const [comment, SetComment] = useState("");
+  const [isValid, SetIsValid] = useState(false);
+  const formik = useFormik({
+    initialValues: { title: "" },
+    onSubmit: (values) => {
+      console.log(values);
+    },
+    validate: (values) => {
+      const errors: FormikErrors<FormValues> = {};
+      if (!values.title) {
+        errors.title = "title is required";
+      }
+      return errors;
+    },
+  });
+  console.log(formik.errors);
+  return (
+    <form onSubmit={formik.handleSubmit}>
+      <div className="max-w-[1040px] mx-auto items-center mt-[52px] flex gap-5">
+        <div className="flex gap-5">
+          <div className="w-[67px] flex flex-col gap-2 py-[100px]">
+            <div className="w-[67px] h-[67px] hover:border-2 hover:border-primaryBlack hover:scale-125">
+              <Image
+                src={image1175}
+                alt=""
+                className="rounded-sm shadow-xl w-full"
+              />
+            </div>
+            <div className="w-[67px] h-[67px] hover:border-2 hover:border-primaryBlack hover:scale-125">
+              <Image src={image1176} alt="" className="rounded-sm shadow-xl" />
+            </div>
+            <div className="w-[67px] h-[67px] hover:border-2 hover:border-primaryBlack hover:scale-125">
+              <Image src={image1177} alt="" className="rounded-sm shadow-xl" />
+            </div>
+            <div className="w-[67px] h-[67px] hover:border-2 hover:border-primaryBlack hover:scale-125">
+              <Image src={image1178} alt="" className="rounded-sm shadow-xl" />
+            </div>
+          </div>
+          <Image
+            src={image1174}
+            alt=""
+            className="rounded-2xl shadow-2xl w-[509px] h-[521px]"
+          />
+        </div>
+        <div className="w-[509px] py-[100px] flex-1 flex-col gap-4">
+          <div className="w-[418px]">
+            <div className="flex flex-col gap-2">
+              <Button
+                variant={"outline"}
+                className="w-14 py-1 px-3 rounded-full"
+              >
+                Шинэ
+              </Button>
+              <div className="text-2xl font-bold flex gap-2 items-center">
+                Wildflower Hoodie
+                <Heart />
+              </div>
+              <p className="text-base font-normal">
+                Зэрлэг цэцгийн зурагтай даавуун материалтай цамц
+              </p>
+            </div>
+          </div>
+          <div className="flex flex-col gap-4">
+            <div className="flex flex-col gap-2">
+              <p className="text-sm font-normal">Хэмжээний заавар</p>
+              <div className="flex gap-1">
+                {/* <div className="flex">
+                <Input type="radio" value="size" name="size" className="text-primaryBlack"/>
+                <Input type="radio" value="size" name="size" />
+                <Input type="radio" value="size" name="size" />
+                <Input type="radio" value="size" name="size" />
+                <Input type="radio" value="size" name="size" />
+                <span className="sr-only">White</span>
+                <span className="size-8 rounded-full border border-black border-opacity-10 bg-white"></span>
+              </div> */}
+                <Button className="w-8 h-8 rounded-full">S</Button>
+                <Button
+                  variant={"outline"}
+                  className="w-8 h-8 text-black rounded-full"
+                >
+                  M
+                </Button>
+                <Button
+                  variant={"outline"}
+                  className="w-8 h-8 text-black rounded-full"
+                >
+                  L
+                </Button>
+                <Button
+                  variant={"outline"}
+                  className="w-8 h-8 text-black rounded-full"
+                >
+                  XL
+                </Button>
+                <Button
+                  variant={"outline"}
+                  className="w-8 h-8 text-black rounded-full"
+                >
+                  2XL
+                </Button>
+              </div>
+            </div>
+            <div className="flex gap-1 items-center">
+              <Button variant={"outline"} className="rounded-full text-2xl">
+                -
+              </Button>
+              <div className="w-8 h-8 items-center flex justify-center">1</div>
+              <Button variant={"outline"} className="rounded-full text-2xl">
+                +
+              </Button>
+            </div>
+          </div>
+          <div className="flex flex-col gap-10 w-[509px]">
+            <div className="flex flex-col gap-2 w-[175px]">
+              <p className="text-xl font-bold">120’000₮</p>
+              <Button
+                variant={"destructive"}
+                className="bg-primaryBlue px-9 py-2 rounded-[20px] text-sm font-medium hover:bg-primaryBlue"
+              >
+                Сагсанд нэмэх
+              </Button>
+            </div>
+            <Popover open={open}>
+              <div className="flex flex-col gap-2">
+                <PopoverTrigger asChild>
+                  <div className="flex gap-1">
+                    <p className="text-sm font-normal">Үнэлгээ</p>
+                    <div
+                      className="text-sm font-normal text-primaryBlue border-b-2 border-primaryBlue w-[97px] hover:cursor-pointer"
+                      onClick={() => SetVisible(true)}
+                    >
+                      бүгдийг харах
+                    </div>
+                  </div>
+                </PopoverTrigger>
+                <div className="flex">
+                  <Star className="size-5 fill-yellow-400 text-yellow-400" />
+                  <Star className="size-5 fill-yellow-400 text-yellow-400" />
+                  <Star className="size-5 fill-yellow-400 text-yellow-400" />
+                  <Star className="size-5 fill-yellow-400 text-yellow-400" />
+                  <Star className="size-5 fill-yellow-400 text-yellow-400" />
+                  <p className="w-[54px] text-sm font-normal">4.6 (24)</p>
+                </div>
+              </div>
+>>>>>>> 59652ed (Formik)
               <PopoverContent className="w-full p-3">
                 <TableBody className="flex flex-col gap-6">
                   <ScrollArea className="h-72 w-[320px] rounded-md border">
@@ -296,7 +470,11 @@ export function ProductDetail() {
                         value={formik.values.title}
                         onChange={formik.handleChange}
                       />
+<<<<<<< HEAD
                       {formik.errors.title ? "Үнэлгээ өгнө үү" : null}
+=======
+                      {formik.errors.title ? "Unelgee ogno uu" : null}
+>>>>>>> 59652ed (Formik)
                     </div>
                     <Button
                       className="bg-primaryBlue rounded-3xl w-32 font-semibold py-2 px-9 hover:bg-blue-800 disabled:opacity-30 disabled:cursor-not-allowed"
@@ -308,10 +486,17 @@ export function ProductDetail() {
                 </TableBody>
               </PopoverContent>
             </Popover>
+<<<<<<< HEAD
 >>>>>>> d3b0300 (Formik onchange update)
           </div>
         </div>
       </div>
     </form >
+=======
+          </div>
+        </div>
+      </div>
+    </form>
+>>>>>>> 59652ed (Formik)
   );
 }
