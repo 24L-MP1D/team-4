7d773258fd086b037255682e54790937cc9530c2
24LP4508 24LP4508 <you@example.com>
2024-10-14 14:51:00 +0800

    ProductDetal

--- e-commerce-frontend/src/app/page.tsx
+++ e-commerce-frontend/src/app/page.tsx
@@ -6,7 +6,6 @@ import { ClientHeader } from "@/components/headerClient";
 import { ProductDetail } from "@/components/clientProductDetail"
 
 import { Header } from "@/components/header";
-import { Userpart } from "@/components/userPart";
 
 export default function Home() {
   return (
